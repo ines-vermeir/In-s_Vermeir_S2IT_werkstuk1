b96cda79bd0baab248eae9853677602c6d2d7e40
Inès Vermeir <ines.vermeir@student.ehb.be>
2018-05-17 11:10:34 +0200

    new project

---
+++ Ines_Vermeir_S2IT_werkstuk1/Ines_Vermeir_S2IT_werkstuk1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ines_Vermeir_S2IT_werkstuk1
+//
+//  Created by VERMEIR Inès (s) on 17/05/2018.
+//  Copyright © 2018 VERMEIR Inès (s). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
